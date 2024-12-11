d60db6f15c03cba93e7151a6dc3373020f24f6d1
viazure <vicrease@gmail.com>
2024-12-11 21:28:54 +0800

    feat: 修改站点源数据

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,17 +11,13 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://running.viazure.cc',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  description: '想不明白的事情那么多，想到就能做的事情太少了。',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://github.com/viazure',
     },
   ],
 };
