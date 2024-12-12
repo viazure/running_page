1ad97df6e54aa8c1afec7e2d7559a60326bd4411
viazure <vicrease@gmail.com>
2024-12-12 21:31:23 +0800

    chore: 再来一点个性化设置。

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,7 +10,7 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running 🏃‍',
+  siteTitle: 'Running‍',
   siteUrl: 'https://run.viazure.cc',
   logo: 'https://avatars.githubusercontent.com/u/9543636?v=4&size=225',
   description: '想不明白的事情那么多，想到就能做的事情太少了。',
